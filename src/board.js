61fc474b72a0f82d02109ff74380ab58f7b9bffc
Nabil Khalid <holanabilkhalid@gmail.com>
2023-06-30 01:08:14 +0800

    Create knightPosition func

--- src/board.js
+++ src/board.js
@@ -1,7 +1,13 @@
 export default function Board() {
-    function createBoard() {
-        return new Array(8).fill(new Array(8).fill(''));
+    let createBoard = Array.from({ length: 8 }, () => new Array(8).fill(''));
+    let prevPosition;
+
+    function knightPosition(arr) {
+        const n = prevPosition;
+        if (prevPosition) createBoard[n[0]][n[1]] = '';
+        prevPosition = [arr[0], arr[1]];
+        createBoard[arr[0]][arr[1]] = 'K';
     }
 
-    return { createBoard };
+    return { createBoard, knightPosition };
 }
